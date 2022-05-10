ecfbff9a82906a7745ff0aacfd2ce994dbdede0e
alihassan-here <alihassanuog50@gmail.com>
2022-05-10 12:15:29 +0500

    image list

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ import './App.css';
 // import { MuiAutocomplete } from './components/MuiAutocomplete';
 // import { MuiLayout } from './components/MuiLayout';
 // import { MuiCard } from './components/MuiCard';
-import { MuiAccordion } from './components/MuiAccordion';
+// import { MuiAccordion } from './components/MuiAccordion';
+import { MuiImageList } from './components/MuiImageList';
 
 function App() {
   return (
@@ -27,7 +28,8 @@ function App() {
       {/* <MuiAutocomplete /> */}
       {/* <MuiLayout /> */}
       {/* <MuiCard /> */}
-      <MuiAccordion />
+      {/* <MuiAccordion /> */}
+      <MuiImageList />
     </div>
   );
 }
