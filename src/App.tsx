7911acf8128a3fe35c75fa810c031786ddf8917e
alihassan-here <alihassanuog50@gmail.com>
2022-06-13 00:04:34 +0500

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,8 @@ import './App.css';
 // import { MuiLayout } from './components/MuiLayout';
 // import { MuiCard } from './components/MuiCard';
 // import { MuiAccordion } from './components/MuiAccordion';
-import { MuiImageList } from './components/MuiImageList';
+// import { MuiImageList } from './components/MuiImageList';
+import { MuiNavbar } from './components/MuiNavbar';
 
 function App() {
   return (
@@ -29,7 +30,8 @@ function App() {
       {/* <MuiLayout /> */}
       {/* <MuiCard /> */}
       {/* <MuiAccordion /> */}
-      <MuiImageList />
+      {/* <MuiImageList /> */}
+      <MuiNavbar />
     </div>
   );
 }
