1280f2bebf7bc3497e0ada4268f885ebda363acb
alihassan-here <alihassanuog50@gmail.com>
2022-06-14 13:58:05 +0500

    dropdown menu

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -1,12 +1,25 @@
-import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from "@mui/material";
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button, Menu, MenuItem } from "@mui/material";
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import React, { useState } from "react";
 
 export const MuiNavbar = () => {
+
+    const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorElement);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorElement(event.currentTarget);
+    }
+    const handleClose = () => {
+        setAnchorElement(null);
+    }
+
     return (
         <AppBar position="static">
             <Toolbar>
                 <IconButton size="large" edge="start" color="inherit" aria-label="logo">
-                    <CatchingPokemonIcon />
+                    <KeyboardArrowDownIcon />
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     POKEMONAPP
@@ -15,8 +28,36 @@ export const MuiNavbar = () => {
                     <Button color="inherit">Features</Button>
                     <Button color="inherit">Pricing</Button>
                     <Button color="inherit">About</Button>
+                    <Button
+                        color="inherit" id="resources-button" onClick={handleClick} aria-control={open ? "resources-menu" : undefined}
+                        aria-haspopup='true'
+                        aria-expanded={open ? "true" : undefined}
+                        endIcon={<KeyboardArrowDownIcon />}
+                    >
+                        Resources
+                    </Button>
                     <Button color="inherit">Login</Button>
                 </Stack>
+                <Menu
+                    id="resources-menu"
+                    anchorEl={anchorElement}
+                    open={open}
+                    MenuListProps={{
+                        'aria-labelledby': 'resources-button'
+                    }}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right'
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right'
+                    }}
+                >
+                    <MenuItem onClick={handleClose}>Blog</MenuItem>
+                    <MenuItem onClick={handleClose}>Podcast</MenuItem>
+                </Menu>
             </Toolbar>
         </AppBar>
 
